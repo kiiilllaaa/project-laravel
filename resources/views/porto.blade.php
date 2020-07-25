dd61bdd935d44c1dbdb03004a421f3fd5bed04bf
Shakila Indah <63502115+kiiilllaaa@users.noreply.github.com>
2020-07-25 22:37:41 +0700

    Program Laravel

---
+++ resources/views/porto.blade.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Portofolio</title>
+
+    <style type="text/css">
+		h3 {
+		    background-color: LightSalmon;
+		}
+ 
+		h2 {
+		    background-color: Salmon;
+		}
+	</style>
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-gray bg-gray">
+  <a class="navbar-brand" href="">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="#profil">Profil</a>
+      <a class="nav-item nav-link" href="#experience">Experience</a>
+      <a class="nav-item nav-link" href="#portofolio">PortoFolio</a>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron bg-secondary">
+
+<center>
+<img src="poto/oo.jpg" width="200px" class="rounded-circle">
+  <h1 class="display-5">Shakila Indah Nurhasti</h1>
+  <p class="lead">Student</p>               
+  <p>SMK TARUNA BHAKTI</p>
+  </center>
+</div>
+
+<div class="container-fluid">
+<div class="row">
+    <div class="col text-center">
+        <h2 id="profil" style="font-family: Arial; font-size: 40px"><b>PROFILE</b></h2><br>
+    </div>
+</div>
+    <p class="col text-center" style="font-size: 20px; font-family: Arial;">
+    Halo Nama saya Shakila Indah Nurhasti. Saya lahir di Depok, 27 Juni 2003 dan Umur saya 17 Tahun. Saya bersekolah di SMK Taruna Bhakti Depok dan saya sekarang berada di kelas 12 jurusan RPL (Rekayasa Perangkat Lunak). Saya Akan Menceritakan Pengelaman Saya Selama Belajar Di SMK Taruna Bhakti
+    </p>
+</div>
+
+<br>
+<br>
+<div class="container">
+    <div class="col text-center">
+        <h2 id="experience" style="font-size 40px; font-family: Arial"><b>Experience</b></h3>
+        <br>
+
+<div class="row">
+    <div class="col md-4">
+        <div class="card">
+            <img src="poto/kunjin.jpg" class="card-img-top">
+                <div class="card-body" style="font-family: Arial; font-size: 35px"><b><h3>Kunjungan Industri (Kunjin)</b></h3>
+                    <p class="card-text" style="font-size: 23px; font-family: Arial;">Kunjungan Industri pada tanggal 4 Januari 2019 di PT Indonesia Comnets Plus (ICON+), yang berlokasi di Jl. Damai No.100, Gandul, Kec. Cinere, Kota Depok, Jawa Barat 16514</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col md-4">
+        <div class="card">
+            <img src="poto/tutor.jpg" class="card-img-top">
+                <div class="card-body" style="font-family: Arial; font-size: 35px;"><b><h3>Tutor Sebaya</b></h3>
+                    <p class="card-text" style="font-size: 23px; font-family: Arial;">Tutor Sebaya Di SMK Taruna Bhakti. Sharing Ilmu Dengan Teman Sekelas/Kakak Kelas</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    </div>
+
+<br>
+<br>
+<div class="container">
+    <div class="col text-center">
+        <h2 id="portofolio" style="font-size 40px; font-family: Arial"><b>PortoFolio</b></h3>
+        <br>
+    <div class="row">
+    <div class="col md-4">
+        <div class="card">
+            <img src="poto/biodata.png" class="card-img-top" width="35px;">
+                <div class="card-body" style="font-family: Arial; font-size: 35px;"><b><h3>Submission Dicoding</b></h3>
+                    <p class="card-text" style="font-size: 23px; font-family: Arial;">Submission Dicoding Membuat Biodata Pribadi</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col md-4">
+        <div class="card">
+            <img src="poto/sertifikat.png" class="card-img-top" width="70px;">
+                <div class="card-body" style="font-family: Arial; font-size: 35px;"><b><h3>Sertifikat Dicoding</b></h3>
+                    <p class="card-text" style="font-size: 23px; font-family: Arial;">Setelah Submission Dicoding Maka Akan Mendapatkan Sertifikat</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col md-4">
+        <div class="card">
+            <img src="poto/web.png" class="card-img-top" width="35px;">
+                <div class="card-body" style="font-family: Arial; font-size: 35px;"><b><h3>Web Sederhana</b></h3>
+                    <p class="card-text" style="font-size: 23px; font-family: Arial;">Membuat Web Sederhana</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col md-4">
+        <div class="card">
+            <img src="poto/trans.png" class="card-img-top" width="35px;">
+                <div class="card-body" style="font-family: Arial; font-size: 35px;"><b><h3>Transaksi Penjualan</b></h3>
+                    <p class="card-text" style="font-size: 23px; font-family: Arial;">Membuat Trasaksi Penjualan Menggunaka NetBeans</p>
+                </div>
+            </div>
+        </div>
+
+</div>
+
+
+
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
